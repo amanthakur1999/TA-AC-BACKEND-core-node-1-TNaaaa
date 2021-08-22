3e1565e8a3e813054799e906d4008793d47844dd
Aman Thakur <uttamthakur2999@gmail.com>
2021-08-23 00:46:43 +0530

    Assignment I

---
+++ block-BNaaaj/app.js
@@ -0,0 +1,8 @@
+var area = require('./area');
+
+var circleAera = area.circle(13);
+
+console.log(circleAera);
+var buffer = Buffer.alloc(12);
+buffer.write('Hello Aman !');
+console.log(buffer.toString());
--- block-BNaaaj/area.js
+++ block-BNaaaj/area.js
@@ -1,4 +1,16 @@
-function square(a, b) {
-  return a * b;
+function square(a) {
+  return a * a;
 }
 square(3, 4);
+
+function ractangle(a, b) {
+  return a * b;
+}
+function circle(r) {
+  2 * 3.24 * r;
+}
+module.exports = {
+  square: square,
+  ractangle: ractangle,
+  circle: circle,
+};
